834098afb7ac2557114b81b98c6bd880b6ebd50b
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2024-02-03 12:42:06 +0530

    Update SpinnerLoader.jsx

--- frontend/src/components/spinner/SpinnerLoader.jsx
+++ frontend/src/components/spinner/SpinnerLoader.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Spinner } from "flowbite-react";
+// import { Spinner } from "flowbite-react";
 
 function SpinnerLoader() {
   return (
     <div className="flex items-center justify-center">
-      {/* <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div> */}
+      <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>
 
-      <Spinner aria-label="Extra large spinner example" size="xl" />
+{/*       <Spinner aria-label="Extra large spinner example" size="xl" /> */}
     </div>
   );
 }
